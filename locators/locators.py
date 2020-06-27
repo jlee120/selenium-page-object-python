732064134390716a089bc72db88de789a46dd8a8
Joan Lee <jolee9123@gmail.com>
2020-06-26 22:12:19 -0700

    Add locators directory with locators file; move locators to this file

---
+++ locators/locators.py
@@ -0,0 +1,10 @@
+class Locators:
+    # login page elements
+    username_textfield_id = 'txtUsername'
+    password_textfield_id = 'txtPassword'
+    login_button_id = 'btnLogin'
+
+    # home page elements
+    welcome_link_id = 'welcome'
+    logout_link_linktext = 'Logout'
+
